5ff03e88be01b29f32aeed34b513738977d08ba5
choguu <danilocoguric12@gmail.com>
2025-11-14 15:50:35 +0100

    chore: test push

--- app/src/main/java/com/vukcho/saigrac/ui/theme/Theme.kt
+++ app/src/main/java/com/vukcho/saigrac/ui/theme/Theme.kt
@@ -21,7 +21,7 @@ private val LightColorScheme =
     lightColorScheme(
         primary = Purple40,
         secondary = PurpleGrey40,
-        tertiary = Pink40
+        tertiary = Pink40,
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
     surface = Color(0xFFFFFBFE),
