e2410810fd25d09467b11e5826719483f6779986
Danilo Čogurić <82438829+choguu@users.noreply.github.com>
2025-11-15 11:19:19 +0100

    Revert "feat: Core navigation logic"

--- app/src/main/java/com/vukcho/saigrac/MainActivity.kt
+++ app/src/main/java/com/vukcho/saigrac/MainActivity.kt
@@ -5,34 +5,49 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.navigation.compose.rememberNavController
-import com.vukcho.saigrac.navigation.AppNavigation
-import com.vukcho.saigrac.navigation.NavigationManager
+import androidx.compose.ui.tooling.preview.Preview
 import com.vukcho.saigrac.ui.theme.SaigracTheme
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-    @Inject
-    lateinit var navigationManager: NavigationManager
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             SaigracTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    AppNavigation(
-                        navController = rememberNavController(),
-                        navigationManager = navigationManager,
-                        innerPadding = innerPadding,
-                        modifier = Modifier,
+
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding),
                     )
                 }
             }
         }
+    }
+}
+
+@Composable
+fun Greeting(
+    name: String,
+    modifier: Modifier = Modifier,
+) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier,
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    SaigracTheme {
+        Greeting("Android")
     }
 }
